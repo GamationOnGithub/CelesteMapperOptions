9859c4bdfd1402c152d5f136e6f54c63dca962ea
GamationOnGithub <52021489+GamationOnGithub@users.noreply.github.com>
2025-07-02 14:38:57 -0700

    make the repo good

---
+++ Source/MapperOptionsMetadata.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Celeste.Mod.MapperOptions
+{
+    public class MapOptionsMetadata
+    {
+        public List<Option> MapOptions { get; set; } = new();
+    }
+
+    public class Option
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public bool Global { get; set; } = true;
+        public string Description { get; set; }
+        public bool StartingBoolValue { get; set; } = false;  
+        public string Values { get; set; }
+        public List<SliderValue> AdvancedValues { get; set; }
+    }
+
+    public class SliderValue
+    {
+        public string Value { get; set; }
+        public string VisibleFlag { get; set; }
+    }
+
+    public class MapperOptionsMetadata
+    {
+        public static TextMenu menu;
+        public static Dictionary<string, Type> PossibleOptionTypes = new()
+        {
+            {"onoff", typeof(TextMenu.OnOff) },
+            {"slider", typeof(TextMenu.Slider) },
+            {"subheader", typeof(TextMenu.SubHeader) }
+        };
+        public static List<Option> MapOptions;
+
+        public static List<Option> TryGetMapperOptionsMetadata(Session session)
+        {
+            MapperOptionsModule.ShouldAddOptions = false;
+
+            if (!Everest.Content.TryGet($"Maps/{session.MapData.Filename}.meta", out ModAsset asset)) return null;
+            if (!(asset?.PathVirtual?.StartsWith("Maps") ?? false)) return null;
+            if (!(asset?.TryDeserialize(out MapOptionsMetadata meta) ?? false)) 
+            {
+                Logger.Error("MapperOptions", "Failed to deserialize meta.yaml. Check your formatting!");
+                return null; 
+            }
+            MapOptions = meta?.MapOptions;
+            
+            foreach (Option option in MapOptions)
+            {
+                if (!MapperOptionsModuleSettings.EnabledOptions.ContainsKey(option.Name)) {
+                    MapperOptionsModuleSettings.EnabledOptions.Add(option.Name, option.Global);
+                }
+                if (option != null && option.Global && MapperOptionsModule.ShouldAddOptions == false)
+                {
+                    // If we have *any* non-null global options, add the menu
+                    MapperOptionsModule.ShouldAddOptions = true;
+                }
+            }
+
+            return MapOptions;
+        }
+
+        public static TextMenu CreateOptionsMenu(Level level)
+        {
+            // Create the menu
+            menu = new TextMenu();
+
+            // Add the fancy cool header
+            menu.Add(new TextMenu.Header(Dialog.Clean("MapperOptions_Title")));
+
+            foreach (Option o in MapOptions)
+            {
+                // Ensure this option is either global (shows up everywhere) or enabled in this zone
+                MapperOptionsModuleSettings.EnabledOptions.TryGetValue(o.Name, out bool enabled);
+                if (enabled)
+                {
+                    // Name and type are required for every option. Everything else is optional and changes per-type
+                    string itemName = Dialog.Clean(o.Name);
+                    string itemType = o.Type.ToLower();
+                    TextMenu.Item item = null;
+
+                    // Get the type and make sure it's valid.
+                    if (!PossibleOptionTypes.TryGetValue(itemType, out Type T)) throw new Exception("type not found");
+
+                    // Construct the menu option based on what the type is.
+                    if (T == typeof(TextMenu.OnOff))
+                    {
+                        // If this is the first time opening this menu, we need to add the option to our Big Dictionary
+                        if (!MapperOptionsModuleSettings.BoolOptions.ContainsKey(o.Name))
+                            MapperOptionsModuleSettings.BoolOptions.Add(o.Name, o.StartingBoolValue);
+
+                        item = new TextMenu.OnOff(itemName, MapperOptionsModuleSettings.BoolOptions.GetValueOrDefault(o.Name, o.StartingBoolValue)).Change(value =>
+                        {
+                            level.Session.SetFlag("MO_" + o.Name, value);
+                            MapperOptionsModuleSettings.BoolOptions[o.Name] = value;
+                        });
+
+                        // If it starts true, make sure the flag starts enabled too
+                        MapperOptionsModuleSettings.BoolOptions.TryGetValue(o.Name, out bool b);
+                        if (b) level.Session.SetFlag("MO_" + o.Name, true);
+                    }
+
+                    else if (T == typeof(TextMenu.Slider))
+                    {
+                        if (o.AdvancedValues == null)
+                        {
+                            // Break our big single string of all the field options down into an array
+                            string[] values = o.Values.Replace(" ", "").Split(',');
+                            if (values.Length == 0)
+                                throw new ArgumentException(
+                                    "Mapper Options: A slider option is specified but has no possible values.");
+                            string[] cleanedValues = new string[values.Length];
+                            for (int i = 0; i < values.Length; i++)
+                            {
+                                cleanedValues[i] = Dialog.Clean(values[i]);
+                            }
+
+
+                            // If this is the first time opening this menu, we need to add the option to our Big Dictionary
+                            if (!MapperOptionsModuleSettings.StringOptions.ContainsKey(o.Name))
+                                MapperOptionsModuleSettings.StringOptions.Add(o.Name, 0);
+
+                            item = new TextMenu.Slider(itemName, (int i) => { return cleanedValues[i]; }, 0,
+                                    values.Length - 1,
+                                    MapperOptionsModuleSettings.StringOptions.GetValueOrDefault(o.Name, 0))
+                                .Change(i =>
+                                {
+                                    for (int j = 0; j < values.Length; j++)
+                                    {
+                                        level.Session.SetFlag("MO_" + o.Name + "_" + values[j], (i == j));
+                                    }
+
+                                    MapperOptionsModuleSettings.StringOptions[o.Name] = i;
+                                });
+
+                            // Make sure to set the flag for the appropriate starting option
+                            level.Session.SetFlag("MO_" + o.Name + "_" + values[0], true);
+                        }
+                        else
+                        {
+                            if (o.AdvancedValues.Count == 0)
+                                throw new ArgumentException("Mapper Options: A slider option is specified but has no possible values.");
+                            List<string> cleanedValues = new();
+                            
+                            for (int i = 0; i < o.AdvancedValues.Count; i++)
+                            {
+                                if (level.Session.GetFlag(o.AdvancedValues[i].VisibleFlag)) 
+                                    cleanedValues[i] = Dialog.Clean(o.AdvancedValues[i].Value);
+                            }
+
+                            string[] cleanedValuesArr = cleanedValues.ToArray();
+                            
+                            if (!MapperOptionsModuleSettings.StringOptions.ContainsKey(o.Name))
+                                MapperOptionsModuleSettings.StringOptions.Add(o.Name, 0);
+
+                            item = new TextMenu.Slider(
+                                itemName,
+                                (int i) => { return cleanedValuesArr[i]; },
+                                0,
+                                o.AdvancedValues.Count - 1,
+                                MapperOptionsModuleSettings.StringOptions.GetValueOrDefault(o.Name, 0))
+                            .Change(i =>
+                            {
+                                for (int j = 0; j < cleanedValuesArr.Length; j++)
+                                { 
+                                    level.Session.SetFlag("MO_" + o.Name + "_" + cleanedValuesArr[j], (i == j));
+                                }
+                                MapperOptionsModuleSettings.StringOptions[o.Name] = i;
+                            });
+                            
+                            level.Session.SetFlag("MO_" + o.Name + "_" + cleanedValuesArr[0]);
+                        }
+                    }
+
+                    else if (T == typeof(TextMenu.SubHeader))
+                    {
+                        item = new TextMenu.SubHeader(itemName);
+                    }
+
+                    else
+                    {
+                        // Something has gone horribly wrong.
+                        throw new InvalidOperationException("Run.");
+                    }
+
+                    // Give everything a description if present. Needs to happen after adding the item to a menu because descriptions are dogshit.
+                    menu.Add(item);
+                    if (o.Description != null) item.AddDescription(menu, Dialog.Clean(o.Description));
+                }
+            }
+
+            // Send back the assembled menu.
+            return menu;
+        }
+    }
+}
---
+++ Source/MapperOptionsModule.cs
@@ -0,0 +1,120 @@
+﻿global using Celeste;
+using Celeste.Mod.Meta;
+using System.Collections.Generic;
+using System;
+using Monocle;
+using System.Linq;
+
+namespace Celeste.Mod.MapperOptions;
+
+public class MapperOptionsModule : EverestModule {
+    public static MapperOptionsModule Instance { get; private set; }
+
+    public override Type SettingsType => typeof(MapperOptionsModuleSettings);
+    public static MapperOptionsModuleSettings Settings => (MapperOptionsModuleSettings) Instance._Settings;
+
+    public override Type SaveDataType => typeof(MapperOptionsModuleSaveData);
+    public static MapperOptionsModuleSaveData SaveData => (MapperOptionsModuleSaveData) Instance._SaveData;
+
+    public List<Option> Options;
+
+    public static bool ShouldAddOptions = false;
+
+    public static TextMenu OptionsMenu;
+
+    public MapperOptionsModule() {
+        Instance = this;
+#if DEBUG
+        // debug builds use verbose logging
+        Logger.SetLogLevel(nameof(MapperOptionsModule), LogLevel.Verbose);
+#else
+        // release builds use info logging to reduce spam in log files
+        Logger.SetLogLevel(nameof(MapperOptionsModule), LogLevel.Info);
+#endif
+    }
+
+    private static void Hook_OnCreatePauseMenuButtons(Level level, TextMenu menu, bool minimal)
+    {
+        if (ShouldAddOptions)
+        {
+            int menuIndex = menu.Items.FindIndex(item =>
+            item.GetType() == typeof(TextMenu.Button) && ((TextMenu.Button)item).Label == Dialog.Clean("menu_pause_savequit"));
+
+            if (menuIndex < 0)
+            {
+                return;
+            }
+
+            // Re-create the menu every time to respect current local options
+            // TODO: Find a better performant way of doing this
+            if (OptionsMenu != null) OptionsMenu = MapperOptionsMetadata.CreateOptionsMenu(level);
+
+            menu.Insert(menuIndex - 1, new TextMenu.Button(Dialog.Clean("MapperOptions_Title"))
+            {
+                OnPressed = () =>
+                {
+                    menu.RemoveSelf();
+
+                    bool comesFromPauseMainMenu = level.PauseMainMenuOpen;
+                    level.PauseMainMenuOpen = false;
+
+                    OptionsMenu.OnESC = OptionsMenu.OnCancel = () =>
+                    {
+                        // close this menu
+                        Audio.Play(SFX.ui_main_button_back);
+
+                        OptionsMenu.Close();
+
+                        // readd original menu
+                        level.Add(menu);
+
+                        level.PauseMainMenuOpen = comesFromPauseMainMenu;
+                    };
+
+                    OptionsMenu.OnPause = () =>
+                    {
+                        Audio.Play(SFX.ui_main_button_back);
+
+                        OptionsMenu.Close();
+
+                        level.Paused = false;
+                        Engine.FreezeTimer = 0.15f;
+                    };
+
+                    level.Add(OptionsMenu);
+
+                    // Make sure the description on the most recently selected item gets added, by force.
+                    // If this is the first time opening the menu, find the first non-(sub)header option and add that description instead.
+                    if (OptionsMenu.Current != null && OptionsMenu.Current.OnEnter != null) OptionsMenu.Current.OnEnter();
+                    else
+                    {
+                        foreach (TextMenu.Item item in OptionsMenu.Items)
+                        {
+                            if (item is TextMenu.Header or TextMenu.SubHeader) continue;
+                            if (item.OnEnter != null) item.OnEnter();
+                            break;
+                        }
+                    }
+                }
+            });
+        }
+    }
+
+    private void Hook_OnLevelLoad(Level level, Player.IntroTypes playerIntro, bool isFromLoader)
+    {
+        if (isFromLoader) {
+            List<Option> options = MapperOptionsMetadata.TryGetMapperOptionsMetadata(level.Session);
+            if (options != null) OptionsMenu = MapperOptionsMetadata.CreateOptionsMenu(level); else return;
+        }
+    }
+
+    public override void Load() {
+        Everest.Events.Level.OnLoadLevel += Hook_OnLevelLoad;
+        Everest.Events.Level.OnCreatePauseMenuButtons += Hook_OnCreatePauseMenuButtons;
+    }
+
+    public override void Unload() {
+        Everest.Events.Level.OnLoadLevel -= Hook_OnLevelLoad;
+        Everest.Events.Level.OnCreatePauseMenuButtons -= Hook_OnCreatePauseMenuButtons;
+    }
+}
